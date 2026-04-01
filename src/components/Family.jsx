38b4fa31b69f35c7de3c0605a41f81663e80f54a
Dayanidhi-N <dayanidhi1093@gmail.com>
2026-04-01 17:49:09 +0530

    footer css changed

--- src/components/Family.jsx
+++ src/components/Family.jsx
@@ -24,7 +24,7 @@ const Ananya = () => {
       </h1>
 
       {/* 📸 Image Grid */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 gap-4">
         {images.map((img, index) => (
           <div
             key={index}
